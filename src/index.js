22ddcaa0f0d9bd2d30b51de79a24a69de1ff14c6
Anuj sharma <60852406+Anujsharma2590@users.noreply.github.com>
2021-03-22 14:36:29 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import reactDom from "react-dom";
+
+//reactDom = require("react-dom");
+
+import "./styles.css";
+
+reactDom.render(<h1>hello world</h1>, document.getElementById("root"));
